3cbd3514b9601a9ffd021652d8d108e7d1d66dd8
ramesh75 <ramesh.katiyar@evivehealth.com>
2020-04-12 01:10:22 +0530

    Removed unused dependencies.

--- src/main/java/com/tech/controller/UserController.java
+++ src/main/java/com/tech/controller/UserController.java
@@ -5,9 +5,7 @@ import com.tech.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -28,6 +26,22 @@ public class UserController {
         UserDetails userDetails = userService.getUserDetails();
         userDetails.setUserId(id);
         return ResponseEntity.ok(userDetails);
+    }
+
+    @PostMapping("/user")
+    public ResponseEntity<?> addUser(@RequestBody UserDetails userDetails){
+        log.info("Adding user with id: {}, Name: {}", userDetails.getUserId(), userDetails.getFirstName());
+        return ResponseEntity.ok("Added User Successfully.");
+    }
+
+    @PutMapping("/user/{id}")
+    public void updateUser(@PathVariable String id, @RequestBody UserDetails userDetails){
+        log.info("Updating user with id: {}, Name: {}", id, userDetails.getFirstName());
+    }
+
+    @DeleteMapping("/user/{id}")
+    public void deleteUser(@PathVariable String id){
+        log.info("Deleting user with id: {}.", id);
     }
 
 }
